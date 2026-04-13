c9b98f02ffa6285d84ea1cfc32f1f4e583680612
Trần Thanh Nhã <nhatran.network@gmail.com>
2026-04-13 17:52:18 +0700

    feat: them web2 Round Robin, /student/ route, prometheus.yml scrape targets

---
+++ MiniCloud/web2/html/assets/js/blog/common.js
@@ -0,0 +1,8 @@
+﻿document.addEventListener('scroll', () => {
+  const bar = document.getElementById('progress-bar');
+  if (!bar) return;
+  const top = document.documentElement.scrollTop || document.body.scrollTop;
+  const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  const percent = height > 0 ? (top / height) * 100 : 0;
+  bar.style.width = percent + '%';
+});
